e13fdc7e3e799dc9b8df70098bf30dff9eeb020c
Thota Ganapathi Rao <thorao1@publicisgroupe.net>
2021-08-04 19:31:44 +0530

    changed images path

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   // public variables
   public imagesList = [
-    {path: '../assets/images/home/carousel-images/offer1.jpg', name : 'offer1'},
-    {path: '../assets/images/home/carousel-images/offer2.jpg', name : 'offer2'},
-    {path: '../assets/images/home/carousel-images/offer3.jpg', name : 'offer3'},
-    {path: '../assets/images/home/carousel-images/offer4.jpg', name : 'offer4'},
-    {path: '../assets/images/home/carousel-images/offer5.jpg', name : 'offer5'},
+    {path: 'assets/images/home/carousel-images/offer1.jpg', name : 'offer1'},
+    {path: 'assets/images/home/carousel-images/offer2.jpg', name : 'offer2'},
+    {path: 'assets/images/home/carousel-images/offer3.jpg', name : 'offer3'},
+    {path: 'assets/images/home/carousel-images/offer4.jpg', name : 'offer4'},
+    {path: 'assets/images/home/carousel-images/offer5.jpg', name : 'offer5'},
   ]
 
   constructor() { }
